f996934a8bc36eae3ab2a51639b0b3182d20ccb0
sh9709 <kidwars82@gmail.com>
2023-02-13 19:24:01 +0900

    02-13 스터디

--- JavaStudy/tangerine.java
+++ JavaStudy/tangerine.java
@@ -10,72 +10,42 @@ import java.util.Map.Entry;
 //01-17 귤고르기
 
 class tangerine {
-    // public static int solution(int k, int[] tangerine) {
-    //     int answer = 0;
-    //     //배열 tangerine을 map에 저장
-    //     Map map = new HashMap<>();
-    //     for(int i = 0; i<tangerine.length; i++){
-    //         if (map.containsKey(Integer.toString(tangerine[i]))){
-    //             int count = (int) map.get(Integer.toString(tangerine[i]));
-    //             map.put(Integer.toString(tangerine[i]),count+1);
-    //         }else{
-    //             map.put(Integer.toString(tangerine[i]),1);
-    //         }
-    //     }
-
-    //     // 내림차순 정렬
-    //     List<Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-    //     Collections.sort(list, new Comparator<Entry<String,Integer>>() {
-    //         public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2){
-    //             return obj2.getValue().compareTo(obj1.getValue());
-    //         }
-    //     });
-
-    //     // 리스트 출력
-    //     // for(Entry<String,Integer> entry : list){
-    //     //     System.out.println(entry.getKey()+":"+entry.getValue()); 
-    //     // }       
-        
-    //     // k만큼 판매할때 몇가지의 경우가 최소인지 계산
-    //     int i = 0;
-    //     while(k > 0){
-    //         k -= list.get(i).getValue();
-    //         answer += 1;
-    //         i++;
-    //     }
-    //     return answer;
-    // }
     public static int solution(int k, int[] tangerine) {
-        Map map = new HashMap();
-        for(int i=0; i < tangerine.length; i++) {
-            if(!map.containsKey(tangerine[i])) {
-                map.put(tangerine[i], 1);
-            }
-            else {
-                map.replace(tangerine[i],(int)map.get(tangerine[i])+1);
-            }
-        }
-        System.out.println(map);
-
-        int[] ase = new int[map.size()];
-        for(int i = 0; i < map.size()-1; i++) {
-            ase[i] = map.;
-        }
-
-        Arrays.sort(ase);
-        System.out.println(ase.length);
-        for(int i=0; i<ase.length; i++)
-            System.out.print(ase[i]);
-
         int answer = 0;
-        for(int i = ase.length-1; k > 0; i--) {
-            k -= ase[i];
-            answer++;
+        //배열 tangerine을 map에 저장
+        Map map = new HashMap<>();
+        for(int i = 0; i<tangerine.length; i++){
+            if (map.containsKey(Integer.toString(tangerine[i]))){
+                int count = (int) map.get(Integer.toString(tangerine[i]));
+                map.put(Integer.toString(tangerine[i]),count+1);
+            }else{
+                map.put(Integer.toString(tangerine[i]),1);
+            }
+        }
+
+        // 내림차순 정렬
+        List<Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, new Comparator<Entry<String,Integer>>() {
+            public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2){
+                return obj2.getValue().compareTo(obj1.getValue());
+            }
+        });
+
+        // 리스트 출력
+        // for(Entry<String,Integer> entry : list){
+        //     System.out.println(entry.getKey()+":"+entry.getValue()); 
+        // }       
+        
+        // k만큼 판매할때 몇가지의 경우가 최소인지 계산
+        int i = 0;
+        while(k > 0){
+            k -= list.get(i).getValue();
+            answer += 1;
+            i++;
         }
         return answer;
     }
    
-
     public static void main(String[] args) {
         int[] tangerine = {3,3,3,2,1,4,2,4,5,3,2,2};
 
