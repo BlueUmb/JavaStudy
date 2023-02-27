3ad56f8fdcf6fb94130d939fc5562e12a47a2475
sh9709 <kidwars82@gmail.com>
2023-02-27 16:07:11 +0900

    02-27 코테

---
+++ JavaStudy/Crain.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Crain {
+
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        ArrayList list = new ArrayList<Integer>();
+
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] answer = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] move = {1,5,3,5,1,2,1,4};
+        System.out.println(solution(answer,move));
+    }
+}
