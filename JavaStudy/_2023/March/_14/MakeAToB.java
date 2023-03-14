2163e1a3b3624b2ce2f5283519985c3d5dd79f7c
seunghoonhan <kidwars82@gmail.com>
2023-03-14 15:50:26 +0900

    03-14 코테

---
+++ JavaStudy/_2023/March/_14/MakeAToB.java
@@ -0,0 +1,48 @@
+package _2023.March._14;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120886'
+//map으로 하지않고 두개의 String을 배열로 만들어 sort시킨후 비교하여 같을경우 1 아닐경우 0리턴하면 편함
+public class MakeAToB {
+    public static int solution(String before, String after) {
+        int answer = 1;
+        String[] beforeArr = before.split("");
+        String[] afterArr = after.split("");
+
+        Map<String,Integer> map = new HashMap<String,Integer>();
+
+        for(String beforeStr : beforeArr){
+            if(map.containsKey(beforeStr)){
+                int value = map.get(beforeStr);
+                map.put(beforeStr, value+1);
+            }
+            else{
+                map.put(beforeStr, 1);
+            }
+            
+        }
+        
+        for(String afterStr : afterArr){
+            if(map.containsKey(afterStr)){
+                int value = map.get(afterStr);
+                map.put(afterStr, value-1);
+            }else{
+                map.put(afterStr, -1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> element : map.entrySet()) {
+            if(element.getValue()!=0){
+                answer = 0;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution("olleh", "hello"));
+    }
+}
---
+++ JavaStudy/_2023/March/_14/MorseCode.java
@@ -0,0 +1,28 @@
+package _2023.March._14;
+
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120838
+public class MorseCode {
+    public static String solution(String letter) {
+        String answer = "";
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] strArr = letter.split(" ");
+        int count=0; 
+
+        for(String str : strArr){
+            count = 0;
+            for(String code : morse){
+                if(!code.equals(str)){
+                    count++;
+                }else{
+                    answer += (char)(count+97);
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(".... . .-.. .-.. ---"));
+    }
+}
