7377bcb4a68385297a5c52f947b3e28a9cac78c4
seunghoonhan <kidwars82@gmail.com>
2023-03-27 16:49:53 +0900

    03-27 코테

---
+++ JavaStudy/_2023/March/_27/OverlapLinesLength.java
@@ -0,0 +1,36 @@
+package _2023.March._27;
+
+import java.util.Arrays;
+
+//Lv0
+//문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120876
+public class OverlapLinesLength {
+    public static int solution(int[][] lines) {
+        int answer = 0;
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i<lines.length; i++){
+            start = Math.min(start, lines[i][0]);
+            end = Math.max(end, lines[i][1]);
+        }
+        
+        for(int i=start; i<end; i++){
+            int count = 0; 
+            for(int j=0; j<lines.length; j++){
+                if(lines[j][0]<=i && lines[j][1] >= i+1){
+                    count++;
+                }  
+            }
+            if(count > 1){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[][] lines = {{-1,1},{1,3},{3,9}};//{{0, 1}, {2, 5}, {3, 9}};//{{0,5},{3,9},{1,10}};
+        System.out.println(solution(lines));
+    }
+}
---
+++ JavaStudy/_2023/March/_27/SafeZone.java
@@ -0,0 +1,50 @@
+package _2023.March._27;
+
+import java.util.Arrays;
+
+//Lv0
+//문제 해결 : https://school.programmers.co.kr/learn/courses/30/lessons/120866
+public class SafeZone {
+    public static int solution(int[][] board) {
+        int[][] newBoard = new int[board.length][board.length];
+
+        for(int i =0; i<board.length; i++){
+            for(int j = 0; j<board[i].length; j++){
+                if(board[i][j] == 1){
+                    danger(i, j, newBoard);
+                }
+            }
+        }
+
+        return (int)Math.pow(board.length, 2) - count(newBoard);
+    }
+
+    public static void danger(int i, int j, int[][] board){
+        board[i][j] = 1;
+        if(i>0){board[i-1][j]=1;}
+        if(i<board.length-1){board[i+1][j]=1;}
+        if(j>0){board[i][j-1]=1;}
+        if(j<board.length-1){board[i][j+1]=1;}
+        if(i>0 && j>0){board[i-1][j-1]=1;}
+        if(i<board.length-1 && j<board.length-1){board[i+1][j+1]=1;}
+        if(i>0 && j<board.length-1){board[i-1][j+1]=1;}
+        if(i<board.length-1 && j>0){board[i+1][j-1]=1;}
+    }
+
+    public static int count(int[][]board){
+        int num = 0;
+
+        for(int[] y : board){
+            for(int x : y){
+                if(x==1){num++;}
+            }
+        }
+
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,1,0,0},{0,0,0,0,0}};
+        System.out.println(solution(board));
+    }
+}
